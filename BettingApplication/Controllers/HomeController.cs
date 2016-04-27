e0c16f2753a890c102fc81d224341986e130d43d
Andreas Wahlberg <andreas.wahlberg@live.se>
2016-04-27 14:57:06 +0200

    Push to git

---
+++ BettingApplication/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BettingApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly SoccerApi.SoccerRestService game = new SoccerApi.SoccerRestService();
+        
+        public ActionResult GetSoccerApi()
+        {
+            return View("index", game.GetSoccerApi());
+        } 
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
